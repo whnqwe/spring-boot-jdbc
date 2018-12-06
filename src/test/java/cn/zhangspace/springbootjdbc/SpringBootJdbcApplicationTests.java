bbcb02f1d4d289532dbc6e1d8f3ab3bde79daa28
zhanghui <whnqwe@126.com>
2018-12-06 20:32:17 +0800

    first commit

---
+++ src/test/java/cn/zhangspace/springbootjdbc/SpringBootJdbcApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.zhangspace.springbootjdbc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootJdbcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
